37d598dcf423c06d97562dd21e52d4a884b9be29
Sourav0809 <spathak7431@gmail.com>
2023-06-05 14:19:46 +0530

    All set

--- srcipt.js
+++ srcipt.js
@@ -3,6 +3,7 @@ let productName = document.querySelector("#productname")
 let productPrice = document.querySelector("#productprice")
 let ul = document.querySelector(".listitems")
 let totalPrice = document.getElementById("total-price")
+let userIcon = document.querySelector(".user-icon")
 
 
 
@@ -11,15 +12,21 @@ let totalPrice = document.getElementById("total-price")
 /*                       Fetching Data On every Refresh                       */
 /* -------------------------------------------------------------------------- */
 
-window.addEventListener("DOMContentLoaded", () => {
-    axios.get("https://crudcrud.com/api/568315bbd24a470aa21f093563dfe8e3/ItemLists")
-        .then((res) => {
-            for (let i = 0; i < res.data.length; i++) {
-                ul.innerHTML += `<li class="lists"><span class="id-token">${res.data[i]._id}</span> <span>${res.data[i].ItemName}</span> <span>${res.data[i].itemPrice}</span> <button class="delete-btn">Delete</button></li>`
-                totalPrice.textContent = Number(totalPrice.textContent) + Number(res.data[i].itemPrice)
-            }
-        })
-        .catch((err) => console.log(err))
+window.addEventListener("DOMContentLoaded", async () => {
+    try {
+        let res = await axios.get(`${JSON.parse(localStorage.getItem("url"))}/ItemLists`)
+
+        for (let i = 0; i < res.data.length; i++) {
+            ul.innerHTML += `<li class="lists"><span class="id-token">${res.data[i]._id}</span> <span>${res.data[i].ItemName}</span> <span>${res.data[i].itemPrice}</span> <button class="delete-btn">Delete</button></li>`
+            totalPrice.textContent = Number(totalPrice.textContent) + Number(res.data[i].itemPrice)
+        }
+
+
+    } catch (error) {
+        console.log(error)
+
+    }
+
 })
 
 
@@ -36,8 +43,8 @@ mainFrom.addEventListener("submit", async (e) => {
             ItemName: productName.value,
             itemPrice: productPrice.value
         }
-
-        const { data } = await axios.post('https://crudcrud.com/api/568315bbd24a470aa21f093563dfe8e3/ItemLists', productObj)
+        // using destructor over here to get the actual data 
+        const { data } = await axios.post(`${JSON.parse(localStorage.getItem("url"))}/ItemLists`, productObj)
         console.log(data)
 
         const enteredName = productName.value
@@ -66,7 +73,8 @@ ul.addEventListener('click', async (e) => {
             if (confirm("Are You Sure ?")) {
                 const li = e.target.parentElement
                 const idToken = li.firstElementChild.innerText
-                await axios.delete(`https://crudcrud.com/api/568315bbd24a470aa21f093563dfe8e3/ItemLists/${idToken}`)
+                await axios.delete(`${JSON.parse(localStorage.getItem("url"))}/ItemLists/${idToken}`)
+
                 totalPrice.textContent = Number(totalPrice.textContent) - Number(li.children[2].textContent)
                 li.remove()
             }
@@ -76,4 +84,16 @@ ul.addEventListener('click', async (e) => {
         console.log(err)
 
     }
+})
+
+
+
+/* -------------------------------------------------------------------------- */
+/*                Adding the network link to the  localStorage                */
+/* -------------------------------------------------------------------------- */
+
+userIcon.addEventListener("click", () => {
+    let url = prompt("Enter the link here.....")
+
+    localStorage.setItem("url", JSON.stringify(url))
 })
